9bc7f61c7b7062d6148b0455fa6629f047aacfc8
Adesegun Adebayo Daniel <adesegun.adebayo@allianz.com>
2020-02-18 08:24:19 +0100

    minimal changes

--- FoodApp_OrderProcessing/Startup.cs
+++ FoodApp_OrderProcessing/Startup.cs
@@ -19,7 +19,6 @@ namespace FoodApp_OrderProcessing
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        [Obsolete]
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
